b2cec9362abf826de49a86a6e965e5c9a1cf2ed2
antmaa <antmaa94@hotmail.com>
2016-02-09 10:56:07 +0200

    moi

---
+++ CarApp/CarApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CarApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    { 
+        
+        public MainPage()
+            
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            int text = int.Parse(textBox1.Text);
+            text++;
+            textBox1.Text = text.ToString();
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int numb = int.Parse(textBox.Text);
+            numb++;
+            textBox.Text = numb.ToString();
+        }
+    }
+}
